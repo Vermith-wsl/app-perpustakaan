e69607ce9c6fe4f62f8623e55865a32ad3d3f6d0
vermith_wsl <prinsadaffa1206@gmail.com>
2026-09-25 09:32:27 +0700

    [P-2] membuat resource controller dan routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
